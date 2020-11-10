07155824b8581b514b98abc5087edac490d8e357
Verneri Välilehto <verneri.valilehto@tuni.fi>
2020-11-10 10:18:36 +0200

    Lisätty parametrien muuttaminen ja hienosäädetty mainwindowia

--- Harkkatyö/MuutaParametreja.xaml.cs
+++ Harkkatyö/MuutaParametreja.xaml.cs
@@ -10,22 +10,31 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Harkkatyö
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MuutaParametreja.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MuutaParametreja : Window
     {
-        public MainWindow()
+        public MuutaParametreja()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
         {
 
         }
