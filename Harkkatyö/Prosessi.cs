7be5537870a6102b4259ca77901815584f7a9f21
Verneri Välilehto <verneri.valilehto@tuni.fi>
2020-12-02 13:21:04 +0200

    Hiottu prosessin metodeita yms

--- Harkkatyö/Prosessi.cs
+++ Harkkatyö/Prosessi.cs
@@ -6,30 +6,94 @@ using System.Threading.Tasks;
 using UnifiedAutomation.UaBase;
 using UnifiedAutomation.UaClient;
 using Tuni.MppOpcUaClientLib;
-
+using System.Windows.Documents;
+using System.Windows;
 
 namespace Harkkatyö
-{ 
+{
+    delegate void ProcessItemsChangedEventHandler(object source, ProcessItemChangedEventArgs args);
     class Prosessi
     {
-        
+
         static private ConnectionParamsHolder parametrit = new ConnectionParamsHolder("opc.tcp://127.0.0.1:8087");
 
         private MppClient asiakas = new MppClient(parametrit);
 
-        public void muutaOnOff(string nimi, bool totuus) 
+        public event ProcessItemsChangedEventHandler jokinMuuttui;
+
+        public void MuutaOnOff(string nimi, bool totuus)
         {
             asiakas.SetOnOffItem(nimi, totuus);
         }
-
-        public void ClientInit() 
+        public void MuutaPumpunOhjaus(string nimi, int ohjaus)
         {
-            asiakas.Init();
+            asiakas.SetPumpControl(nimi, ohjaus);
+        }
+        public void MuutaVenttiilinOhjaus(string nimi, int ohjaus)
+        {
+            asiakas.SetValveOpening(nimi, ohjaus);
         }
 
+        public void ClientInit()
+        {
+            asiakas.Init();
+            AddSubscriptions();
+        }
+        private void AddSubscriptions()
+        {
+            // Vaihtoehdot subscriptioneiden lisäämiseen, kumpi onkaan oikea vaihtoehto
+
+            //sasiakas.AddToSubscription("jokinMuuttui");
+            
+            /*
+            foreach (string element in mitattavatBool.Keys)
+            {
+                asiakas.AddToSubscription(element);
+            }
+            foreach (string element in mitattavatDouble.Keys)
+            {
+                asiakas.AddToSubscription(element);
+            }
+            foreach (string element in mitattavatInt.Keys)
+            {
+                asiakas.AddToSubscription(element);
+            }*/
+        }
+        private void MuutaMuuttuneet(object source, ProcessItemChangedEventArgs args)
+        {
+            Dictionary<string, MppValue> kaikkiMuuttuneet = args.ChangedItems;
+            foreach (string itemName in kaikkiMuuttuneet.Keys)
+            {
+                MppValue item = kaikkiMuuttuneet[itemName];
+
+                if ((int)item.ValueType == 0)
+                {
+                    bool arvo = (bool)item.GetValue();
+                }
+                if ((int)item.ValueType == 1)
+                {
+                    double arvo = (double)item.GetValue();
+                }
+                if ((int)item.ValueType == 2)
+                {
+                    int arvo = (int)item.GetValue();
+                }
+            }
+        }
+        /*
         // Säiliöiden pinnankorkeudet
+        private int LI100;
         private int LI200;
-        
+        private int LI400;
+
+        // Pintavahdit
+        private bool LS200N;
+        private bool LS300N;
+        private bool LS300P;
+
+        // Pumppujen esivalinta
+        private bool P100_P200_PRESET;
+
         // Pumppujen tilat
         private int P100;
         private int P200;
@@ -38,16 +102,37 @@ namespace Harkkatyö
         private int TI100;
         private int TI300;
 
-        // Säätöventtiilien tilat
-        private int V102;
-        private int V104;
+        // Mitattavat paineet
+        private int PI300;
 
         // Virtaus ennen P100
         private double FI100;
 
         // T100 pintahälytys
         private bool LA100;
+        */
 
+        // Dictit, joissa on eri tyyppiset mitattavat arvot
+        private Dictionary<string, bool> mitattavatBool = new Dictionary<string, bool>() {
+            { "LS-200", false },
+            { "LS-300", false },
+            { "LS+400", false}
+        };
+
+        private Dictionary<string, int> mitattavatInt = new Dictionary<string, int>() {
+            { "LI100", 0},
+            { "LI200", 0},
+            { "LI400", 0},
+            { "TI100", 0},
+            { "TI300", 0},
+            { "PI300", 0}
+        };
+
+        private Dictionary<string, double> mitattavatDouble = new Dictionary<string, double>() {
+            { "FI100", 0}
+        };
+
+        /*
         // Sulkuventtiilien tilat
         private bool V101;
         private bool V103;
@@ -67,7 +152,12 @@ namespace Harkkatyö
         private bool V403;
         private bool V404;
 
+        // Säätöventtiilien tilat
+        private int V102;
+        private int V104;
+        */
+
     }
-    
-    
+
+
 }
